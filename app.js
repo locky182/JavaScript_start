9d5b93dd444c5de95fca45abf91c8698ef30c363
locky182 <oven8209@gmail.com>
2023-01-09 13:27:45 +0300

    арифметические операции, конкатенация строк

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-console.log('Hello world')
-  
-      
+const width = 10;
+const height = 5;
+const space = width * height;
+console.log(space);
+const volume = 2**3;// Тоже самое что 2*2*2
+console.log(volume);
+
+//строки конкатинация
+const city = 'Москва';
+const street = 'Новослободская';
+console.log(city + ', ' + street + ' ' + 5);
