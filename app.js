646032d0691ba340a058820241aa4d849a1e175f
locky182 <oven8209@gmail.com>
2023-01-09 14:37:42 +0300

    4.5 Типы данных

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-//Операторы присваивания
-let age = 18 + 2;
-age++;// можно age += 2 это равно как age = age + 2
-age += 4;// age = age + 4
-age *= 6;// age = age * 6
-age /= 2;// age = age / 2
-age++;
-console.log(age);
-
-//Операторы срвнения
-const vasya = 20;
-console.log(age > vasya);
-console.log(age >= vasya);
-console.log(age < vasya);
-console.log(age <= vasya);
-console.log(age == vasya);
+let a = 5;
+let b = 4.5;
+console.log(typeof (b));
+console.log(typeof (a));
+b = 'Hello';
+console.log(typeof (b));
+let isAdmin = false;
+console.log(typeof (isAdmin));
+let isBigger = 5>2;
+console.log(isBigger);
+console.log(typeof (isBigger));
+let c;
+console.log(typeof (c));//undefined потому что ничего не присвоили
+let d = null;// это пустое значение, оно как бы есть и является пустым
+//Например если нам ничего в запросе не вернуло то можноприсвоить null
+console.log(typeof (d));//как тип gjrfpsdftn object
+console.log(typeof d == null);// false сначала выполняется typeof d
+//а потом сравнивает object and null равно false
