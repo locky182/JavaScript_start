d4021c83ef4ba0de4c06493c76131a9c652f54b4
locky182 <oven8209@gmail.com>
2023-01-09 15:53:52 +0300

    4.7 Шаблонные строки

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
-let a = 5;
-let b = 4.5;
-console.log(typeof (b));
-console.log(typeof (a));
-b = 'Hello';
-console.log(typeof (b));
-let isAdmin = false;
-console.log(typeof (isAdmin));
-let isBigger = 5>2;
-console.log(isBigger);
-console.log(typeof (isBigger));
-let c;
-console.log(typeof (c));//undefined потому что ничего не присвоили
-let d = null;// это пустое значение, оно как бы есть и является пустым
-//Например если нам ничего в запросе не вернуло то можноприсвоить null
-console.log(typeof (d));//как тип gjrfpsdftn object
-console.log(typeof d == null);// false сначала выполняется typeof d
-//а потом сравнивает object and null равно false
+//Шаблонные строки
+const projectName = 'Сайт магазина';
+const price = 2000;
+const author = 'Василий Пупкин';
+
+//const template = author + ' заказал '
+ //+ projectName + ' по цене '
+  //+ price + ' рублей';
+  const template2 = `${author} заказал ${projectName} по цене ${price} рублей`
+
+console.log(template2);
+//переход на другую строку без спецсимвола \n 
+const template3 = `Цена: 2000
+Товар: nokia 3110`;
+console.log(template3);
