ffe6fbe0a3dbc39316808d6764404d4559e76ddf
locky182 <oven8209@gmail.com>
2023-01-09 18:08:23 +0300

    4.8 Преобразование типов

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
-//Шаблонные строки
-const projectName = 'Сайт магазина';
-const price = 2000;
-const author = 'Василий Пупкин';
-
-//const template = author + ' заказал '
- //+ projectName + ' по цене '
-  //+ price + ' рублей';
-  const template2 = `${author} заказал ${projectName} по цене ${price} рублей`
-
-console.log(template2);
-//переход на другую строку без спецсимвола \n 
-const template3 = `Цена: 2000
-Товар: nokia 3110`;
-console.log(template3);
+let age = '18';
+console.log(Number(age) + 5);
+console.log(age - 5);
+console.log(age / 3);
+console.log(typeof age);
+console.log(String(4) + 7);//конкатенация
+let name = 'Vasya';
+console.log(Number(name) + 5);//NaN
+console.log(Boolean(1));//true
+console.log(Boolean(0));//false
+console.log(Boolean(123));//true
+console.log(Boolean(-1));//true
+//строка пустая = false
+//строка заполнена true
+console.log('This is strings');
+console.log(Boolean('dddddd'));//true
+console.log(Boolean(''))//false
+console.log(Boolean(' '));//true это строка содержит пробел,поэтому не пустая
+console.log(Boolean('') + 1);//boolean-false-0-0+1=1
