e71bfe90bc8e31cd98012e3d5e8d7281367536e0
locky182 <oven8209@gmail.com>
2023-01-10 20:52:44 +0300

    6.5 Упражнение - Проверка прав

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-let age = 0;
-console.log(age||18);//age = 0, false
-console.log(age??18);// age = 0, true
+const balance = 10000;
+const bonusBalance = 90;
+let isBanned = false;
+let isExist = false;
+let isSelling = true;
+// if((balance > 1000 || bonusBalance > 100) 
+//     && (!isBanned) 
+//     && (!isExist) 
+//     && (isSelling)){
+//         console.log('Вы сможете купить игру')
+//     }else{
+//         console.log('Вы не сможете купить игру');
+//     }
+
+let isBuy = ((balance >1000)||(bonusBalance>90))
+&&!isBanned
+&&!isExist
+&&isSelling;
+console.log(isBuy? 'Yes':'No');
