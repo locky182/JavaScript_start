a55ec819b589d523214579d51c217eda8b053892
locky182 <oven8209@gmail.com>
2023-01-10 11:16:19 +0300

    5.1 If else

--- app.js
+++ app.js
@@ -1,19 +1,9 @@
-let age = '18';
-console.log(Number(age) + 5);
-console.log(age - 5);
-console.log(age / 3);
-console.log(typeof age);
-console.log(String(4) + 7);//конкатенация
-let name = 'Vasya';
-console.log(Number(name) + 5);//NaN
-console.log(Boolean(1));//true
-console.log(Boolean(0));//false
-console.log(Boolean(123));//true
-console.log(Boolean(-1));//true
-//строка пустая = false
-//строка заполнена true
-console.log('This is strings');
-console.log(Boolean('dddddd'));//true
-console.log(Boolean(''))//false
-console.log(Boolean(' '));//true это строка содержит пробел,поэтому не пустая
-console.log(Boolean('') + 1);//boolean-false-0-0+1=1
+const money = 100;
+let ourMoney = 40;
+if(ourMoney >= money){
+    console.log('Нам хватает денег на покупку');
+}else if(ourMoney > 23){
+    console.log('Куплен мини продукт');
+}else{
+    console.log('Нам не хватает денег на покупку');
+}
