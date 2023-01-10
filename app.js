027d023b58abc8b1d13dc8db313988db3c86aeda
locky182 <oven8209@gmail.com>
2023-01-10 18:01:16 +0300

    6.2 Логические операторы

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-let check = prompt(`Сколько будет 7 + или - 15`);
-switch(true){
-    case check === 'Я не робот':
-    case Number(check) === 22:
-    case Number(check) === -8:
-        console.log('Успех');
-        break;
-    default:
-        console.log('Извините, вы робот');            
+const isAdmin = true;
+const isSuperAdmin = false;
+
+console.log(`Править файлы ${isAdmin&&isSuperAdmin}`);
+//&& -> и
+// || -> или
+// ! -> не
+if(isAdmin||isAdmin){
+    console.log('Write');
 }
-
-
+ 
