48d7844a8a165a7edf7c5d5b30da9a14e7d31b8b
locky182 <oven8209@gmail.com>
2023-01-10 12:03:16 +0300

    5.3 Операторы равенства

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-const money = 100;
-let ourMoney = 40;
-if(ourMoney >= money){
-    console.log('Нам хватает денег на покупку');
-}else if(ourMoney > 23){
-    console.log('Куплен мини продукт');
+const secretNumber = 7;
+if(secretNumber == 7){//Проверяет только значение
+    console.log('Угадал. Не строгое равенство');
+}
+
+if(secretNumber === 7){//Проверяет еще  и значение, и тип!!!!
+    console.log('Угадал. Строгое равенство!!');
+}
+//А если строка то '7' == 7 true
+//                  '7' === 7 false
+//                  Number('7') === 7 true
+
+let num = prompt('Input num: ');
+//let num = Number(prompt('Input num: ')); можно сразу оборачивать считывание строки с браузера
+if(num === 7){// false, но если Number(num) === 7 true
+//if(Number(num) === 7)true    
+    console.log('All are ok!!');
+
 }else{
-    console.log('Нам не хватает денег на покупку');
+    console.log('It is not ok');
 }
