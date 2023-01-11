6e121d22655284e9c9e543904ca0f5219abcf955
locky182 <oven8209@gmail.com>
2023-01-11 12:56:51 +0300

    8.1 Знакомство с массивами

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-function canAccesWebsite(age){
-    if(age>18){
-        return true;
-    }else{
-        return false;
-    }
-}
+const role1 = 'user';
+const role2 = 'admin';
+const role3 = 'superUser';
 
-const isCan = canAccesWebsite(17);
-//console.log(isCan);
-
-const canAccesWebsite2 = (age) => age < 18 ? 'No': 'Yes';
-console.log(canAccesWebsite2(16));
+const roles = ['user', 'admin', 'superUser'];//массив строк
+const userInfo = ['Anna', 25];
+console.log(roles);
+console.log(roles[0]);
+console.log(roles.length);//это свойство массива -> длина
+console.log(roles[roles.length - 1]);//получение последнего элемента массива
+console.log(roles.at(0))//новая фича по выведению элементов
+console.log(roles.at(-1));//получение последнего элемента в массиве
+const usersAge = [2022 - 2021, 10>0? '5': '10'];// можно и так, а смысл?
+console.log(usersAge);
+const userNames = new Array('Vasya', 'Petya');//Еще один способ
+console.log(userNames);
