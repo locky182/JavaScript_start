3fb4223d3b7f6bd83dcb7f987efbc0cd843b56b5
locky182 <oven8209@gmail.com>
2023-01-11 10:10:30 +0300

    7.5 Параметры по умолчанию

--- app.js
+++ app.js
@@ -1,18 +1,5 @@
-//Обычная функция
-function powerOfTwo(num){
-    return num * num;
-}
-console.log(powerOfTwo(2));
-
-//Стрелочная функция сокращенная запись
-//Стрелочная функция также не может всплывать
-const poft = (num) => num * num;
-console.log(poft(4));
-
-//расширенная запись
-
-const movt = (num) =>
-{    
-    return num **2;
-};
-console.log(movt(2));
+//Параметры по умолчанию 2
+//Второй параметр поумолчанию
+//если мы вообще не передаем второй аргумент
+const toPowerArrow = (num, power = 2) => num**power;
+console.log(toPowerArrow(2, 3));
