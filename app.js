d64805e335f456d2abd37ddd3b5a8f7c4b050eb5
locky182 <oven8209@gmail.com>
2023-01-11 18:19:26 +0300

    8.4 Slice, splice, concat, reverse

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
-const roles = ['admin', 'user', 'writer'];
-const indexOfRoles = roles.indexOf('user');
-console.log(indexOfRoles);
+// const roles = ['user', 'admin', 'superUser', 'manager'];
+// const result = roles.slice(2);//данный метод не изменяет массив
+// console.log(result);//выводит все с 2 элемента
+// console.log(roles);
 
-if(roles.indexOf('admin') >= 0){//если нет такого то возвращает -1
-    console.log('Hello admin!!!');
-}
 
-console.log(roles.includes('admin'));//возвращает true, если есть
+// const result1 = roles.slice(2,3);//данный методне изменяет массив
+// console.log(result1);
+// console.log(roles);//выводит с 2 элемента не включая 3
 
-console.log(roles.includes
-    ('superMario')? 'Hi admin': 'getta fuck out hier!');
+// const result2 = roles.slice(-1);//берет последний элемент массива и добавляет в переменную object
+// //const result2 = roles.slice(-1)[0];//Это будет уже чисто string
+// console.log(result2);//если -2 то берет два последних элемента из массива
+// console.log(roles);
+// console.log(typeof(result2));
+// console.log(roles.at(-1));//это уже строка
+// //slice возвращает object
+//+++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//Метод splice
+ const roles = ['user', 'admin', 'superUser', 'manager'];
+//  const result = roles.splice(2);
+//  console.log(result);//возврат что вырезано
+//  console.log(roles);//реально изменяет массив['user','admin']
+ 
+// const result = roles.splice(2,1);//2-элемент, 1-количество элементов после
+// console.log(result);
+// console.log(roles);
+//++++++++++++++++++++++++++++++++++++++++
+//reverse()
+// const result = roles.reverse();
+// console.log(result);//изменяет массив, переворачивает
+// console.log(roles);
+//+++++++++++++++++++++++++++++++++++
+//concat()-склеивает массивы
+
+
+
+
+
+
