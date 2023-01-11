0e6f9f132caed033aec1c0bd2e240c97b820e2a0
locky182 <oven8209@gmail.com>
2023-01-11 09:30:26 +0300

    7.1 Введение в функции

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-const balance = 10000;
-const bonusBalance = 90;
-let isBanned = false;
-let isExist = false;
-let isSelling = true;
-// if((balance > 1000 || bonusBalance > 100) 
-//     && (!isBanned) 
-//     && (!isExist) 
-//     && (isSelling)){
-//         console.log('Вы сможете купить игру')
-//     }else{
-//         console.log('Вы не сможете купить игру');
-//     }
+function fromMilesToKm(miles){
+    const km = miles/0.62137;
+    return km;
 
-let isBuy = ((balance >1000)||(bonusBalance>90))
-&&!isBanned
-&&!isExist
-&&isSelling;
-console.log(isBuy? 'Yes':'No');
+}
+
+let resultOfKm = fromMilesToKm(4);
+console.log(resultOfKm);
+console.log(resultOfKm);
+console.log(resultOfKm);
+console.log(resultOfKm);
+console.log(resultOfKm);
+
+function logName(name, surname){
+    console.log(`Мое имя ${name}, фамилия ${surname}`);
+}
+
+logName('Alexey', 'Liahov');
