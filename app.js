ca9720bf83b9cb83e448b1270a7a911a54c51df3
locky182 <oven8209@gmail.com>
2023-01-11 14:08:19 +0300

    8.3 Поиск элемента

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
-const users = ['Anna', 'Vitya', 'Katya'];
-console.log(users);
-users[2] = 'Kristina';
-console.log(users);
-// users[3] = 'Nikita';//без проблемдобавил еще элемент
-// console.log(users);
-// users[8] = 'Sasha';//если добавляем через позиции то появляется дыры-empty
-// console.log(users);
+const roles = ['admin', 'user', 'writer'];
+const indexOfRoles = roles.indexOf('user');
+console.log(indexOfRoles);
 
-users.push('Nikita');//добавление элемента в конец
-const arrayLength = users.push('Vasya');
-console.log(arrayLength);
-console.log(users);
-users.unshift('Grisha');//unshift добавление в начало массива
-console.log(users);
-users.pop();//удаляет последний элемент из массива,также возвращает удаленный элемент
-console.log(users);
-users.shift();//удаление первого элемента в массиве
-console.log(users);
+if(roles.indexOf('admin') >= 0){//если нет такого то возвращает -1
+    console.log('Hello admin!!!');
+}
+
+console.log(roles.includes('admin'));//возвращает true, если есть
+
+console.log(roles.includes
+    ('superMario')? 'Hi admin': 'getta fuck out hier!');
