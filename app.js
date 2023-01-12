a5248cd65e532a8802b4cd08126aaea80480bce6
locky182 <oven8209@gmail.com>
2023-01-12 18:34:11 +0300

    9.3 Упражнение - Цикл в обратном порядке

--- app.js
+++ app.js
@@ -1,18 +1,9 @@
-const url = 'https://purpleschool.ru/course/javascript';
+const arr = ['!', 'JS', 'люблю', 'Я'];
+    const myMass = [];
+for(let i = arr.length - 1; i >= 0; i--){
+  myMass.push(arr[i]);
 
-//Протокол (https)
-//Доменное имя (purpleschool.ru)
-//Путь внутри сайта (/course/javascript)
-
-function getUrlParts(url){
-const [protocol, _, host, ...path] = url.split('/');
-if(protocol === 'https:'|| protocol ==='http:'){
-    console.log(protocol, host, path);
-    console.log(`Протокол ${protocol.split(':')[0]}`);
-    console.log(`Доменное имя ${host}`);
-    console.log(`Путь внутри сайта /${path.join('/')} `);
 
 }
-}
-
-getUrlParts(url);
+console.log(myMass.join(' '));
+//arr.reverse().join(' '); это более краткая запись
