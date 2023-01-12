5d654f48a4a5ce13db1923b7d792b3c9c5e87a7a
locky182 <oven8209@gmail.com>
2023-01-12 18:53:07 +0300

    9.4 Цикл в цикле

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
-const arr = ['!', 'JS', 'люблю', 'Я'];
-    const myMass = [];
-for(let i = arr.length - 1; i >= 0; i--){
-  myMass.push(arr[i]);
-
-
+const task = [[1,2,3,4],[5,6,7,8],[9,10,11,12]];
+for(let i = 0; i < task.length; i++){
+    for(let j = 0; j < task[i].length; j++){
+        console.log(task[i][j]);
+    }
 }
-console.log(myMass.join(' '));
-//arr.reverse().join(' '); это более краткая запись
