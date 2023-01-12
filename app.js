1f0896bf096a72385b9ba6256bffca0ea7593058
locky182 <oven8209@gmail.com>
2023-01-12 07:33:20 +0300

    8.5 Из строки в массив и обратно

--- app.js
+++ app.js
@@ -1,41 +1,10 @@
-// const roles = ['user', 'admin', 'superUser', 'manager'];
-// const result = roles.slice(2);//данный метод не изменяет массив
-// console.log(result);//выводит все с 2 элемента
-// console.log(roles);
-
-
-// const result1 = roles.slice(2,3);//данный методне изменяет массив
-// console.log(result1);
-// console.log(roles);//выводит с 2 элемента не включая 3
-
-// const result2 = roles.slice(-1);//берет последний элемент массива и добавляет в переменную object
-// //const result2 = roles.slice(-1)[0];//Это будет уже чисто string
-// console.log(result2);//если -2 то берет два последних элемента из массива
-// console.log(roles);
-// console.log(typeof(result2));
-// console.log(roles.at(-1));//это уже строка
-// //slice возвращает object
-//+++++++++++++++++++++++++++++++++++++++++++++++++++
-
-//Метод splice
- const roles = ['user', 'admin', 'superUser', 'manager'];
-//  const result = roles.splice(2);
-//  console.log(result);//возврат что вырезано
-//  console.log(roles);//реально изменяет массив['user','admin']
- 
-// const result = roles.splice(2,1);//2-элемент, 1-количество элементов после
-// console.log(result);
-// console.log(roles);
-//++++++++++++++++++++++++++++++++++++++++
-//reverse()
-// const result = roles.reverse();
-// console.log(result);//изменяет массив, переворачивает
-// console.log(roles);
-//+++++++++++++++++++++++++++++++++++
-//concat()-склеивает массивы
-
-
-
-
-
-
+// разбить строку на массив строк
+const url = 'auth/user/login';
+const res =  url.split('/');
+console.log(res);
+console.log(res.at(-1));// получили последний элемент
+//+++++++++++++++++++++++++++++++++++++++++
+const roles = ['user', 'admin', 'superUser'];
+const res2 = roles.join('-');//из массива в строку
+console.log(res2);
+console.log(typeof(res2))
