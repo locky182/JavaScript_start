a36a017b2173104f7840a8e1299c5eb26d7f504d
locky182 <oven8209@gmail.com>
2023-01-12 08:44:08 +0300

    8.6 Упражнение - Обновление списка задач

--- app.js
+++ app.js
@@ -1,10 +1,43 @@
-// разбить строку на массив строк
-const url = 'auth/user/login';
-const res =  url.split('/');
-console.log(res);
-console.log(res.at(-1));// получили последний элемент
-//+++++++++++++++++++++++++++++++++++++++++
-const roles = ['user', 'admin', 'superUser'];
-const res2 = roles.join('-');//из массива в строку
-console.log(res2);
-console.log(typeof(res2))
+//задача
+
+//const task = ['задача 1'];
+//добавление задачи в конец списка
+//метод push()
+// function addTaskToEnd(myArrayTask, task){
+//     result = myArrayTask.push(task);//push меняет исходный массив
+//     console.log(myArrayTask);
+
+// }
+
+// const tasks = ['task 1'];
+// addTaskToEnd(tasks, 'task 2');
+// console.log(tasks);
+//+++++++++++++++++++++++++++++++++++++++++++++
+//удаление задачи по названию 
+
+const tasks2 = ['user', 'admin', 'superUser'];
+function delTaskForName(myArrayTask, task){
+    const index = myArrayTask.indexOf(task);//может удалять по индексу
+    if(index === -1){
+        return;//благодаря return мы выйдем полностью из функции 
+    }//и не будем выполнятькод ниже
+    myArrayTask.splice(index,1);
+    console.log(myArrayTask);
+}
+
+
+delTaskForName(tasks2, 'user');
+console.log(tasks2);//массив изменен
+//++++++++++++++++++++++++++++++++++++++++++++++
+//Перенос задачи в начало списка по названию
+// function carryTaskToTheStart(myArrayTask, task){
+//     const res = myArrayTask.unshift(task);
+//     myArrayTask.splice(-1);
+//     console.log(myArrayTask);
+// }
+   
+
+
+// const tasks2 = ['user', 'admin', 'superUser'];
+// carryTaskToTheStart(tasks2, 'superUser' );
+// console.log(tasks2);//массив изменен
