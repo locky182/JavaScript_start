8827d727b2746c619e99f1a7d343f897c54a3a50
locky182 <oven8209@gmail.com>
2023-01-13 14:15:02 +0300

    10.4 Упражнение - Стрелочные функции

--- app.js
+++ app.js
@@ -1,19 +1,2 @@
-// function power(pow){
-//     return function(num){
-//         return num**pow;
-//     }
-// }
-
-// // const powerOfTwo = power(4);//данная переменная и является
-// // вложенной анонимной функцией
-// console.log(powerOfTwo(5));
-//console.log(power(3)(2));//это болеекороткая запись
-//каждая скобка идет в глубь вложенной функции 
-
-function power(num){
-    return function(pow){
-        return num**pow;
-    }
-}
-
+const power =(num)=> (pow)=>num**pow;//стрелочная функция
 console.log(power(2)(3));
