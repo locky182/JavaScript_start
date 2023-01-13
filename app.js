9efbfa5860f6cca34adea84442e91ac8bd74cf64
locky182 <oven8209@gmail.com>
2023-01-13 11:02:10 +0300

    9.6 Циклы for of и for in

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-const task = [[1,2,3,4],[5,6,7,8],[9,10,11,12]];
-for(let i = 0; i < task.length; i++){
-    for(let j = 0; j < task[i].length; j++){
-        console.log(task[i][j]);
-    }
+const arr = [1,2,3,4,5,6,7,8];
+
+for(let element of arr){//итерация по значению
+    console.log(element);//вывод массива
+   
 }
+
+for(let index in arr){//for in это итерация по индексу
+    console.log(index);
+}
+
