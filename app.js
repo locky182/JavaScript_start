0960e7211fa97be37fd55d1bc22dd49c10d1479e
locky182 <oven8209@gmail.com>
2023-01-13 12:34:32 +0300

    9.7 Упражнение - Расчёт итогового баланса

--- app.js
+++ app.js
@@ -1,11 +1,58 @@
-const arr = [1,2,3,4,5,6,7,8];
+//Итогового баланса
+//Наличия отрицательного баланса (если после очередной операции баланс < 0, то выдавать false)
+//Расчёта среднего расхода и среднего дохода
 
-for(let element of arr){//итерация по значению
-    console.log(element);//вывод массива
-   
+const operations = [1000, -700, 300, -500, 10000];
+let accumulyatorOfBalance = 0;
+let totalBalance = 0;
+let plus = [];
+let minus = [];
+let sumOfPlus = 0;
+let sumOfMinus = 0;
+
+function myTotalBalance(operations){
+for(let i = 0; i < operations.length; i++){
+
+    accumulyatorOfBalance = accumulyatorOfBalance
+     + operations[i];
+    
+
+
+}
+ totalBalance = accumulyatorOfBalance + 100;
+     console.log(totalBalance);
 }
 
-for(let index in arr){//for in это итерация по индексу
-    console.log(index);
-}
+ function averageOfBalance(operations){
+        let averageOfPlus;
+        let averageOfMinus;
+        for(let i = 0; i < operations.length; i++){
+            if(operations[i] > 0){
+                plus.push(operations[i])
+
+            }
+        }
+        for(let i = 0; i < plus.length; i++){
+            sumOfPlus = sumOfPlus + plus[i];
+
+        }
+
+        averageOfPlus = sumOfPlus / plus.length;
+
+        for(let i = 0; i < operations.length; i++){
+            if(operations[i] < 0){
+                minus.push(operations[i]);
+            }
+        }
+        for(let i = 0; i < minus.length; i++){
+            sumOfMinus = sumOfMinus + minus[i];
+        }
+        averageOfMinus = sumOfMinus / minus.length;
+
+        console.log(`Средний положительный доход ${averageOfPlus}`);
+        console.log(`Средний отрицательный доход ${averageOfMinus}`);
+    }
+
+myTotalBalance(operations);
+averageOfBalance(operations);
 
