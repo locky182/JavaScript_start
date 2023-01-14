d01e1d67f360ad112afc1f84a25ec8724da1e170
locky182 <oven8209@gmail.com>
2023-01-14 12:31:22 +0300

    11.5 reduce

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
-//если массив одинарный то мы просто на прямую работаем с элементами
-//если двойной то уточняем []
-//задача получить результирующий массив
-//положительной разници элементов
-const prices = [[100, 200],[120,100],[200,350]];
-const result = prices
-    .map((product) => product[1] - product[0])
-    .filter((price) => price > 0);
-    console.log(result);
+//reduce это сужение до одного значения
+// return accumulyator + value()-текущеезначение
+//не обязательно суммировать, можно выводитьминимальный элемент
 
+const balance = [45,12,56,87];
+
+// let sumOfBalanse = 0;
+
+// for(const el  of balance){
+//   sumOfBalanse += el;
+// }
+// console.log(sumOfBalanse);
+ 
+// const totalBalance = balance
+//     // .reduce((accum, elem) => accum + elem );
+//     // console.log(totalBalance);
+//     .reduce((accumulate, valueOfBalance)=>{
+//       return accumulate += valueOfBalance;
+//     },0);// 0 после тела функции это чему равен аккумулятор изначально
+
+//     console.log(totalBalance);//сложение
+
+    //поиск минимального элемента
+    let minBalance = balance
+    
+    .reduce((accumulate, valueOfBalance)=>{
+      if(valueOfBalance > accumulate){
+        return accumulate;
+
+      }else{
+        return valueOfBalance;
+      }
+      
+    });
+
+    console.log(minBalance);
