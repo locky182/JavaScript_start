f566b1a53738fe52196c02703882ba267461df10
locky182 <oven8209@gmail.com>
2023-01-14 15:00:58 +0300

    11.7 find и findIndex

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
-//вывести среднее число массива
-//суммировать и разделить на количество элементов
-const arr = [1,4,4,10];
-const result = arr
-    .reduce((accum, el, i) =>{
-      if(i != arr.length-1){
-        return accum += el;
-      }else{
-        return (accum +el)/arr.length;
-      }
-    },0)
-
- console.log(result);
+const arr = [1,3,5,8,9];
+//найти первый элемент > 5 = 8
+//если не найдет этот элемент то он будет undefined
+const firstGT5 = arr.find(elem => elem > 5);
+const firstIndexGT5 = arr.findIndex(elem => elem > 3);
+console.log(firstGT5);//8
+console.log(firstIndexGT5);//2 индекс элемента который больше 3
+//если индекс не найден то -1 мы элемент не нашли 
+//если индекс элемента === -1 то его нет
