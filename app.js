f0febfea942e2453a2b468e8eec851dc93dc85ac
locky182 <oven8209@gmail.com>
2023-01-14 14:40:58 +0300

    11.6 Упражнение - среднее значение

--- app.js
+++ app.js
@@ -1,36 +1,13 @@
-//reduce это сужение до одного значения
-// return accumulyator + value()-текущеезначение
-//не обязательно суммировать, можно выводитьминимальный элемент
-
-const balance = [45,12,56,87];
-
-// let sumOfBalanse = 0;
-
-// for(const el  of balance){
-//   sumOfBalanse += el;
-// }
-// console.log(sumOfBalanse);
- 
-// const totalBalance = balance
-//     // .reduce((accum, elem) => accum + elem );
-//     // console.log(totalBalance);
-//     .reduce((accumulate, valueOfBalance)=>{
-//       return accumulate += valueOfBalance;
-//     },0);// 0 после тела функции это чему равен аккумулятор изначально
-
-//     console.log(totalBalance);//сложение
-
-    //поиск минимального элемента
-    let minBalance = balance
-    
-    .reduce((accumulate, valueOfBalance)=>{
-      if(valueOfBalance > accumulate){
-        return accumulate;
-
+//вывести среднее число массива
+//суммировать и разделить на количество элементов
+const arr = [1,4,4,10];
+const result = arr
+    .reduce((accum, el, i) =>{
+      if(i != arr.length-1){
+        return accum += el;
       }else{
-        return valueOfBalance;
+        return (accum +el)/arr.length;
       }
-      
-    });
+    },0)
 
-    console.log(minBalance);
+ console.log(result);
