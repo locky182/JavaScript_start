b7ad4aae3b0b5c9602fcc9e047738c72f2f5b0a8
locky182 <oven8209@gmail.com>
2023-01-14 07:52:17 +0300

    11.3 filter

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
-//map() это преобразование элемента
-//возвращает нам новый массив
-//map только return
-//trunsuctionInRub это массив
-const trunsuctionInUsd = [10, 490, 23, 45,21];
-const trunsuctionInRub = trunsuctionInUsd
-.map((truns) =>{
-  return truns * 60;
-    
-});
-console.log(trunsuctionInUsd);
-console.log(trunsuctionInRub);
-console.log(typeof(trunsuctionInRub));
+//filter фильтрация данных
+//возвращает новый массив
+//оставитьэлементы которые удовлетворяют этому условию
+//возвращает значение если true
+//фильтрует все что false
+const operations = [-40,1,-56,2,-54];
+// let positivOperations = [];
+// for(operatia of operations){
+  
+//   if(operatia > 0){
+//     positivOperations.push(operatia);
+
+//   } 
+
+// }
+// console.log(positivOperations);
+
+// const positivOperations = operations
+// .filter((operatia) =>{
+//     return operatia > -40;
+// });
+// console.log(positivOperations);
+
+//это приминение сразу filter and map
+const positiveOperationInRub = operations
+    .filter((operatia) => operatia > 0)
+    .map((operatia) => operatia * 60);
+
+    console.log(positiveOperationInRub);
