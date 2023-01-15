db6451305d31084cb125689c1809aba7fc373fa7
locky182 <oven8209@gmail.com>
2023-01-15 09:20:36 +0300

    12.2 Базовые методы

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-//создание массивов
-const arr = new Array(4);//количество элементов
-console.log(arr);//empty пустой массив
-console.log(arr.fill(1));//массив заполнен единицами
-//++++++++++++++++++++++++++++++++++++
-const arr2 =  Array.from({length: 5},(cursor,i)=> i+1)
-console.log(arr2);
+//работа со строками
+//строка это массив букв
+
+const firstName = 'Вася Пупкинс';
+console.log(firstName[4]);//' '
+console.log(firstName[5]);//'П'
+console.log(firstName[0]);//'В'
+console.log(firstName.charAt(0));//'В'
+const lastName = 'Кукушкина'[0];//можно и так
+console.log(lastName);
+console.log(firstName.length);//11, пробел посчитан как символ
+console.log(firstName.indexOf('а'));//индекс первой встречающейся буква а
+console.log(firstName.lastIndexOf('с'));//последний индекс посл встреч
+console.log(firstName.includes('я'));//true
+console.log(firstName.slice(4));//' ' + 'Пупкинс'обрезано до
+console.log(firstName.slice(5));//'Пупкинс' отрезано с 5 индекса
+console.log(firstName.slice(5,8));//'Пуп' все обрезано с 5-8 исключая 8
+console.log(firstName);
