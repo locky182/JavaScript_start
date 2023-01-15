0a8a3c875b7e9f1c66ebc18f2ac9ca6e3d175a1b
locky182 <oven8209@gmail.com>
2023-01-15 10:12:29 +0300

    12.3 Упражнение - выделение имени

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-//работа со строками
-//строка это массив букв
+//Задача на выделение имени
+//Вывод 'Вася Пупкин'
+let userName = 'Вася aka Terminator Пупкин';
+// let res = userName.split(' ');
+// res.splice(1,2);
+// const fullName = res.join(' ');
+// console.log(fullName);
 
-const firstName = 'Вася Пупкинс';
-console.log(firstName[4]);//' '
-console.log(firstName[5]);//'П'
-console.log(firstName[0]);//'В'
-console.log(firstName.charAt(0));//'В'
-const lastName = 'Кукушкина'[0];//можно и так
-console.log(lastName);
-console.log(firstName.length);//11, пробел посчитан как символ
-console.log(firstName.indexOf('а'));//индекс первой встречающейся буква а
-console.log(firstName.lastIndexOf('с'));//последний индекс посл встреч
-console.log(firstName.includes('я'));//true
-console.log(firstName.slice(4));//' ' + 'Пупкинс'обрезано до
-console.log(firstName.slice(5));//'Пупкинс' отрезано с 5 индекса
-console.log(firstName.slice(5,8));//'Пуп' все обрезано с 5-8 исключая 8
-console.log(firstName);
+const firstName = userName.slice(0, userName.indexOf(' '));
+console.log(firstName);//возвращает строку в указанном диапозоне 'Вася'
+console.log(typeof(firstName));
+const lastName = userName
+    .slice(
+     userName.lastIndexOf(' ') + 1, userName.length);
+     console.log(lastName);
+
+     console.log(`${firstName} ${lastName}`);
+
