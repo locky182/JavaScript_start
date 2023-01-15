bd7e51317cb8a6f56e65d9a44f433e743d4c518f
locky182 <oven8209@gmail.com>
2023-01-15 22:18:00 +0300

    12.7 Дополнение строк

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
-//12.4 Преобразование строки
-const str = 'Вася Пупкина';
-console.log(str.startsWith('В'));//true
-console.log(str.endsWith('н'));//true
-console.log(str.toLowerCase());//'вася пупкин'
-console.log(str.includes('у'));//
-console.log(str.replace('В','Д'));
-console.log(str.replace(/а/g, 'и'));// это regex /а/g -все совпадения меняет на 'и'
-//обычно реплейс меняет только первое совпадение
-
-//+++++++++++++++++++++++++++++++++
-const str2 = '   Hi, how are      you   ';
-console.log(str2.trim());//чистит строку от пробелов
-//чистит перед первым словом и после конечного слова
+//12.7 Дополнение строк
+const film = 'Звездные войны ';
+console.log(film.padStart(20, '*'));//******Звездные войны
+console.log(film.padEnd(20,'*'));
+console.log(film.padStart(20,'*').repeat(10));//повтор 10 раз
