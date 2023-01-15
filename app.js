e9bf07f488c29aee56ef3c3b074d840ba43106e8
locky182 <oven8209@gmail.com>
2023-01-15 23:11:01 +0300

    12.8 Упражнение - маскировка карты

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
-//12.7 Дополнение строк
-const film = 'Звездные войны ';
-console.log(film.padStart(20, '*'));//******Звездные войны
-console.log(film.padEnd(20,'*'));
-console.log(film.padStart(20,'*').repeat(10));//повтор 10 раз
+//замаскировать все кроме последних 4 символов
+//************5678
+const cart = '1234567812345678';
+const cart1 = '1234123412348888';
+
+function maskCreditCard(cart){
+//   let res = cart.split('');
+//   let delArr = res.splice(12,4);
+//   //console.log(res);
+//   let str = delArr.join(''); 
+//   console.log(str.padStart(16,'*'));
+//+++++++++++++++++++++++++++++++++++++++
+//Решение преподавателя
+console.log(cart.slice(-4).padStart(16,'*'));//возвращает 4 последних элемента и прибавляет вначало*******
+   }
+   
+
+
+
+maskCreditCard(cart1);
+
