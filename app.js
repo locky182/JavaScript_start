f225ba35f7165dcc4bcd68528994753b25e58979
locky182 <oven8209@gmail.com>
2023-01-16 10:23:40 +0300

    13.5 Методы объектов

--- app.js
+++ app.js
@@ -1,24 +1,12 @@
-//преобразование обьектов
-//преобразовать до вида {fullNmame: 'Вася Пупкин', scillNum: 2}
-const users = [
-   {
-   name: 'Вася',
-   surname: 'Пупкин',
-   age: 30,
-   skills: ['Разработка', 'DevOps']
-   },
-   {
-   name: 'Катя',
-   surname: 'Белова',
-   age: 18,
-   skills: ['Дизайн']
+//метод функции является свойством котрый содержит значение 
+const user = {
+   firstName: 'Alexey',
+   lastName:'Liahov',
+   age: 37,
+   getUser: function(){
+      return this.firstName + ' ' + this.lastName;
+
    }
-];
+};
 
-const userData = users.map((user)=>{
-   return {
-      fullName: `${user.name} ${user.surname}`,
-   scillNum: `${user.skills.length}`};
-});
-
-console.log(userData);
+console.log(user.getUser());
