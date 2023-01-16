35b3c97b6a08e585921dc87eae3267b629057408
locky182 <oven8209@gmail.com>
2023-01-16 08:09:54 +0300

    13.1 Знакомство с объектами

--- app.js
+++ app.js
@@ -1,21 +1,11 @@
-//замаскировать все кроме последних 4 символов
-//************5678
-const cart = '1234567812345678';
-const cart1 = '1234123412348888';
-
-function maskCreditCard(cart){
-//   let res = cart.split('');
-//   let delArr = res.splice(12,4);
-//   //console.log(res);
-//   let str = delArr.join(''); 
-//   console.log(str.padStart(16,'*'));
-//+++++++++++++++++++++++++++++++++++++++
-//Решение преподавателя
-console.log(cart.slice(-4).padStart(16,'*'));//возвращает 4 последних элемента и прибавляет вначало*******
-   }
-   
-
-
-
-maskCreditCard(cart1);
-
+//обьекты
+//ключ:значение
+//значение-> метод, массив, другой обьект, строка
+const user = {
+   firstName: 'Вася',
+   lastName: 'Пупкин',
+   age: 24,
+   skills:['Программирование',
+           'готовка'
+         ]
+};
