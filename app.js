0d7374903776d94dd2b9c0c4e8aa7a28252d1b5e
locky182 <oven8209@gmail.com>
2023-01-16 08:40:05 +0300

    13.2 Обращение к элементам

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
-//обьекты
-//ключ:значение
-//значение-> метод, массив, другой обьект, строка
+
 const user = {
    firstName: 'Вася',
    lastName: 'Пупкин',
    age: 24,
    skills:['Программирование',
            'готовка'
-         ]
+         ],
+         eduPro: 'MFTI'
 };
+console.log(typeof(user));//object
+console.log(user);
+console.log(user.firstName);
+console.log(user.skills[0]);
+console.log(user);
+console.log(user['skills']);//подходит для рассчетов
+console.log(user.skills);//простой вывод
+let str = 'edu';
+console.log(user[str + 'Pro'])//MFTI
+
+user['city'] = 'Moscow';//присвоение дополнительного ключ:значение
+user.fruitLike = 'qiwi';//так тоже созд ключ:значение
+console.log(user);
+user.age = 37;//модификация обьекта
+console.log(user.age);
