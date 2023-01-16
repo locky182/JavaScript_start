5686f280543aeceb274f90e063762be708469d50
locky182 <oven8209@gmail.com>
2023-01-16 14:03:31 +0300

    13.6 Упражнение - кошелёк

--- app.js
+++ app.js
@@ -1,12 +1,52 @@
-//метод функции является свойством котрый содержит значение 
-const user = {
-   firstName: 'Alexey',
-   lastName:'Liahov',
-   age: 37,
-   getUser: function(){
-      return this.firstName + ' ' + this.lastName;
+//13.6 Упражнение - кошелёк
+//метод увеличения и уменьшения баланса
+//{reason: 'налог', sum: -100}
+//возвращает true or false еслибаланса не достаточно
+//метод числа операций по кошельку
+
+const wallet = {
+   balance: 0,
+   operations: [],
+
+   increase: function(sum, reason){
+      this.balance += sum;
+      this.operations.push({reason: reason,
+      sum: sum});
+      return true;
+
+   },
+
+   countOfOperations: function(){
+      return this.operations.length;
+
+   },
+
+   decrease: function(sum,reason){
+      if(this.balance > sum){
+         this.balance -= sum;
+         this.operations.push({reason: reason,
+      sum: sum});
+
+         return true;
+      }else{
+         return false;
+      }
 
    }
+  
 };
 
-console.log(user.getUser());
+ console.log(wallet.increase(100,'Зарплата'));
+ console.log(wallet.countOfOperations());
+ console.log(wallet.increase(1000,'Левак'));
+ console.log(wallet.countOfOperations());
+ console.log(wallet.increase(3000,'Продажа'));
+ console.log(wallet.countOfOperations());
+ console.log(wallet.decrease(10000000, 'Покупка ноутбука'));
+ console.log(wallet.operations);
+
+
+
+
+
+
