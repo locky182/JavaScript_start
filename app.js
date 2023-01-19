1388b619d4fcea62d22677d8b3e5a10fb1d18182
locky182 <oven8209@gmail.com>
2023-01-19 11:43:25 +0300

    16 Замыкание

--- app.js
+++ app.js
@@ -1,62 +1,17 @@
-const warehous = {
-   
-   goods: [],
-   findGoodsById: function(id){
-      return this.goods
-      .find(g => g.id == id);
-      
-     
-   },
-//Добавление товара
-   addGoods: function(good){
-      const existedGood = this.findGoodsById(good.id)
-      if(existedGood){
-         console.log(`Этот товар уже есть на складе`);
-         return;
-      
-      }
-         this.goods.push(good);
-      
+'use strict'
+//Замыкание
 
-      
-   },
-   getWeightKg: function(){
-      return this.goods.reduce((acc, el)=>{
-         acc += el.weight?.kg ? el.weight.kg : 0;
-         //если вес есть? то прибавляем вес если нет то прибавляем 0
+function changeBalance(){
+    let balance = 0;
+    return function(sum){
+        balance += sum;
+        console.log(`Баланс: ${balance}`)
 
-      },0)
-   }
-};
+    }
+}
 
-/* goods */
-const car = {
-   id: 1,
-   weight: {
-      kg: 1000
-   },
-   brand: 'ford'
-
-};
-
-const chair = {
-   id: 2,
-   weight: {
-      kg: 2
-   }
-};
-
-const paper = {
-   id: 3,
-   color: 'red'
-}; 
-
-warehous.addGoods(car);
-warehous.addGoods(car);
-warehous.addGoods(chair);
-let ourel = warehous.findGoodsById(1);
-console.log(ourel);
-
-
-
-console.log(warehous.goods);
+const change = changeBalance();
+change(100);
+change(50);
+change(15);
+console.dir(change);
