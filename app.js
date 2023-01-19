bc810321ea3778011df5f230351888638f99fde9
locky182 <oven8209@gmail.com>
2023-01-19 15:00:45 +0300

    16.7 Упражнение - работа с замыканиями

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
-'use strict'
-//Замыкание
+'use strict';
+const userInfo = {
+    balance: 0,
+    operations: 0,
+    increase: function(sum){
+        this.balance += sum;
+        this.operations++;
 
-function changeBalance(){
-    let balance = 0;
-    return function(sum){
-        balance += sum;
-        console.log(`Баланс: ${balance}`)
+    }
 
+};
+
+function user(){
+    const userObj ={
+    balance: 0,
+    operations: 0,
+    increase: function(sum){
+        this.balance += sum;
+        this.operations++;
+
+       }
+    }
+    return function(){
+        return userObj;
     }
 }
 
-const change = changeBalance();
-change(100);
-change(50);
-change(15);
-console.dir(change);
+const user1 = user();
+user1().increase(100);
+console.log(user1());
